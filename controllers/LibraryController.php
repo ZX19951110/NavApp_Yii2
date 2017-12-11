279f54488dac4f65af979ef4489fbf1349617c00
ZX19951110 <619186893@qq.com>
2017-12-11 16:52:53 +0100

    add functions

--- controllers/LibraryController.php
+++ controllers/LibraryController.php
@@ -8,6 +8,8 @@
 namespace app\controllers;
 use app\models\User;
 use Yii;
+use yii\db\Exception;
+use yii\db\IntegrityException;
 use yii\web\Controller;
 use app\models\Library;
 class LibraryController extends Controller{
@@ -19,5 +21,42 @@ class LibraryController extends Controller{
     }
     public function actionCreate(){
         $reqArr = Yii::$app->request->post();
+        $name = $reqArr['name']?$reqArr['name']:'blank';
+        $codice_biblioteca = $reqArr['codice_biblioteca']?$reqArr['codice_biblioteca']:'blank';
+        $email = $reqArr['email']?$reqArr['email']:'blank';
+        $address = $reqArr['address']?$reqArr['address']:'blank';
+        $ia_id = $reqArr['ia_id']?$reqArr['ia_id']:'blank';
+        $id = $reqArr['id']?$reqArr['id']:'blank';
+        $latitude = $reqArr['latitude']?(float)$reqArr['latitude']:0.00;
+        $longitude = $reqArr['longitude']?(float)$reqArr['longitude']:0.00;
+        $telephone = $reqArr['telephone']?$reqArr['telephone']:'blank';
+        $library = new Library();
+        $library->name = $name;
+        $library->codice_biblioteca = $codice_biblioteca;
+        $library->email = $email;
+        $library->id = $id;
+        $library->latitude = $latitude;
+        $library->longitude = $longitude;
+        $library->telephone = $telephone;
+        $library->ia_id = $ia_id;
+        $library->address = $address;
+        //$sql = "insert into library values ($ia_id,$id,$latitude,$longitude,$name,$telephone,$email,$codice_biblioteca,$address)";
+        /*if ($library->save()){
+            return json_encode(['status'=>0,'message'=>'create success']);
+        }*/
+        try{
+            $library->save();
+            return json_encode(['status'=>0,'message'=>'create success']);
+        }catch (IntegrityException $exception){
+            return json_encode(['status'=>1,'message'=>'Duplicate Primary Key']);
+        }catch (Exception $exception){
+            return json_encode(['status'=>1,'message'=>'database connect error!']);
+        }
+
+    }
+    public function actionSearch(){
+        $name = Yii::$app->request->get('name');
+        $target = Library::find()->where(['name'=>$name])->asArray()->all();
+        return json_encode($target);
     }
 }
--- models/Library.php
+++ models/Library.php
@@ -3,15 +3,12 @@
 namespace app\models;
 use yii\db\ActiveRecord;
 class Library extends ActiveRecord {
-    public $address;
-    public $codice_biblioteca;
-    public $email;
-    public $ia_id;
-    public $id;
-    public $latitude;
-    public $longitude;
-    public $name;
-    public $telephone;
+    public function rules()
+    {
+        return [
+            [['address',  'codice_biblioteca','email','ia_id','id','name','telephone'], 'required'],
+        ];
+    }
     public static function find(){
         return new LibraryQuery(get_called_class());
     }
